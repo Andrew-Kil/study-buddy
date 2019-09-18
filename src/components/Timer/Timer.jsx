78858654a197e4c040e789e7f0a7cffaaa4908d1
Andrew KIl <kil.andrew@gmail.com>
2019-09-18 00:49:45 -0400

    countdown timer: start, pause/play & reset

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -4,7 +4,7 @@ export const Timer = props => {
   return (
     <div>
       <h1>
-        {props.minutes} :{" "}
+        {props.minutes < 10 ? "0" + props.minutes : props.minutes} :{" "}
         {props.seconds < 10 ? "0" + props.seconds : props.seconds}
       </h1>
     </div>
