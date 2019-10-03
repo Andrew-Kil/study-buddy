bc52e49f6b5d1f1a56d8fb9300d02280cf2952c5
Andrew KIl <kil.andrew@gmail.com>
2019-10-02 22:01:31 -0400

    revert TasksContainer to class component

--- src/components/Tasks/TasksContainer.jsx
+++ src/components/Tasks/TasksContainer.jsx
@@ -1,43 +1,41 @@
-import React, { useState } from "react";
-
+import React, { Component } from "react";
 import { Tasks } from "./Tasks.jsx";
-
 import "./Tasks.scss";
 
-export const TasksContainer = () => {
-  const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState([]);
-
-  const updateTask = e => {
-    setTask(e.target.value);
+export default class TasksContainer extends Component {
+  state = {
+    task: "",
+    tasks: []
   };
 
-  const updateTasks = task => {
-    setTasks(tasks => [...tasks, task]);
-  };
-
-  const handleSubmit = e => {
+  handleSubmit = e => {
     e.preventDefault();
-    updateTasks(task);
-    setTask("");
+    this.updateTasks(this.state.task);
+    this.setState({ task: "" });
   };
 
-  const deleteTask = item => e => {
-    const filteredTasks = tasks.filter(task => task !== item);
-    setTasks(filteredTasks);
+  updateTask = e => this.setState({ task: e.target.value });
+
+  updateTasks = task => this.setState({ tasks: [...this.state.tasks, task] });
+
+  deleteTask = item => e => {
+    const filteredTasks = this.state.tasks.filter(task => task !== item);
+    this.setState({ tasks: filteredTasks });
   };
 
-  return (
-    <>
-      <form onSubmit={e => handleSubmit(e)}>
-        <input
-          type="text"
-          placeholder="Add task"
-          className="input-add-task"
-          value={task}
-          onChange={e => updateTask(e)}></input>
-      </form>
-      <Tasks tasks={tasks} deleteTask={deleteTask}></Tasks>
-    </>
-  );
-};
+  render() {
+    return (
+      <div>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Add task"
+            className="input-add-task"
+            value={this.state.task}
+            onChange={e => this.updateTask(e)}></input>
+        </form>
+        <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask}></Tasks>
+      </div>
+    );
+  }
+}
